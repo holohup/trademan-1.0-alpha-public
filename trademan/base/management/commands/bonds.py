beab4ee93d1b19c0f204a9792d10406d1c4ca0d8
Alexey Shapiro <academ@gmail.com>
2023-02-22 08:51:49 +0200

    flake8

--- trademan/base/management/commands/bonds.py
+++ trademan/base/management/commands/bonds.py
@@ -9,7 +9,9 @@ from tinkoff.invest.utils import quotation_to_decimal
 
 from base.models import Figi
 
-if not all([settings.TCS_RO_TOKEN, settings.TCS_RW_TOKEN, settings.TCS_ACCOUNT_ID]):
+if not all(
+    [settings.TCS_RO_TOKEN, settings.TCS_RW_TOKEN, settings.TCS_ACCOUNT_ID]
+):
     message = (
         'Не удалось загрузить все переменные из окружения. Переменные:\n'
         f'TCS_ACCOUNT_ID: {settings.TCS_ACCOUNT_ID}\n'
@@ -21,15 +23,19 @@ if not all([settings.TCS_RO_TOKEN, settings.TCS_RW_TOKEN, settings.TCS_ACCOUNT_I
 
 def prevalidate_instrument(inst: any([Share, Future]), _type: str) -> bool:
     if _type == 'Share':
-        return all((
+        return all(
+            (
                 inst.real_exchange == RealExchange.REAL_EXCHANGE_MOEX,
                 quotation_to_decimal(inst.min_price_increment) > 0,
-        ))
+            )
+        )
     if _type == 'Future':
-        return all((
-            inst.real_exchange == RealExchange.REAL_EXCHANGE_RTS,
-            quotation_to_decimal(inst.min_price_increment) > 0,
-        ))
+        return all(
+            (
+                inst.real_exchange == RealExchange.REAL_EXCHANGE_RTS,
+                quotation_to_decimal(inst.min_price_increment) > 0,
+            )
+        )
 
 
 def fill_fields(inst: any([Share, Future])) -> dict:
@@ -41,7 +47,7 @@ def fill_fields(inst: any([Share, Future])) -> dict:
         'api_trading_available': inst.api_trade_available_flag,
         'short_enabled': inst.short_enabled_flag,
         'buy_enabled': inst.buy_available_flag,
-        'sell_enabled': inst.sell_available_flag
+        'sell_enabled': inst.sell_available_flag,
     }
 
 
@@ -52,8 +58,8 @@ class Command(BaseCommand):
         received_figi = set()
         result_message = ''
         with RetryingClient(
-                settings.TCS_RO_TOKEN,
-                RetryClientSettings(use_retry=True, max_retry_attempt=10)
+            settings.TCS_RO_TOKEN,
+            RetryClientSettings(use_retry=True, max_retry_attempt=10),
         ) as client:
             try:
                 response_stocks = client.instruments.shares()
@@ -71,20 +77,30 @@ class Command(BaseCommand):
                         new_values = fill_fields(share)
                         new_values.update(type='S')
                         received_figi.add(share.figi)
-                        tcs_stock, _ = Figi.objects.update_or_create(figi=share.figi, defaults=new_values)
+                        tcs_stock, _ = Figi.objects.update_or_create(
+                            figi=share.figi, defaults=new_values
+                        )
 
                 for future in response_futures.instruments:
                     if prevalidate_instrument(inst=future, _type='Future'):
                         new_values = fill_fields(future)
                         new_values.update(type='F')
-                        new_values.update(basic_asset_size=int(quotation_to_decimal(future.basic_asset_size)))
+                        new_values.update(
+                            basic_asset_size=int(
+                                quotation_to_decimal(future.basic_asset_size)
+                            )
+                        )
                         received_figi.add(future.figi)
-                        tcs_future, _ = Figi.objects.update_or_create(figi=future.figi, defaults=new_values)
+                        tcs_future, _ = Figi.objects.update_or_create(
+                            figi=future.figi, defaults=new_values
+                        )
 
                 if clean_up_flag:
                     for figi in Figi.objects.all():
                         if figi.figi not in received_figi:
-                            result_message += f'Deleting figi with ticker {figi}\n'
+                            result_message += (
+                                f'Deleting figi with ticker {figi}\n'
+                            )
                             figi.delete()
             finally:
                 return result_message
