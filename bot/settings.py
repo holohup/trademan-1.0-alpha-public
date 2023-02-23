745101139b48aedeb0e1932d3fc4d2daf0762fbc
Alexey Shapiro <academ@gmail.com>
2023-02-23 13:14:00 +0200

    default aiogram token for test purposes

--- bot/settings.py
+++ bot/settings.py
@@ -44,7 +44,7 @@ load_dotenv()
 TCS_RO_TOKEN = os.getenv('TCS_RO_TOKEN', '000')
 TCS_RW_TOKEN = os.getenv('TCS_RW_TOKEN', '000')
 TCS_ACCOUNT_ID = os.getenv('ACCOUNT_ID', '000')
-TELEGRAM_TOKEN = os.getenv('TG_TOKEN', '000')
+TELEGRAM_TOKEN = os.getenv('TG_TOKEN', '123456789:AABBCCDDEEFFaabbccddeeff-1234567890')
 TELEGRAM_CHAT_ID = int(os.getenv('TG_CHAT_ID', '000'))
 
 # endpoints
