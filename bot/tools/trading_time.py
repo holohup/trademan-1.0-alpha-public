bd94a03b3528ba4a9483f5feb481574c88c52b29
Alexey Shapiro <academ@gmail.com>
2023-03-09 08:22:06 +0200

    time class finished

--- bot/tools/trading_time.py
+++ bot/tools/trading_time.py
@@ -1,56 +1,154 @@
 import zoneinfo
 from datetime import datetime, time, timedelta
+from typing import NamedTuple
 
 from bot.tools.classes import Asset
 
-TRADE_DAYS = (0, 4)
-MORNING_TRADE_HOURS = {'morning': (time(9, 00), time(9, 59, 59, 59))}
-EVENING_TRADE_HOURS = {'evening': (time(19, 5), time(23, 50))}
-STOCKS_TRADE_HOURS = {'base': (time(10, 00), time(18, 39, 59))}
-FUTURES_TRADE_HOURS = {
-    'before_clearing': (time(10, 00), time(14, 00)),
-    'after_clearing': (time(14, 5), time(18, 45)),
-}
-TIME_OFFSET = timedelta(seconds=30)
-ZONE = zoneinfo.ZoneInfo('Europe/Moscow')
 
-OFFSET_ADJUSTED_WORK_HOURS = {
-    key: (
-        (
-            datetime.combine(datetime.now(ZONE).date(), value[0]) + TIME_OFFSET
-        ).time(),
-        (
-            datetime.combine(datetime.now(ZONE).date(), value[1]) - TIME_OFFSET
-        ).time(),
-    )
-    for key, value in STOCKS_TRADE_HOURS.items()
+class TradingSession(NamedTuple):
+    open: time
+    close: time
+
+
+TRADE_DAYS = (0, 4)
+MORNING_TRADE_HOURS = {'morning': TradingSession(time(9, 00), time(9, 50))}
+EVENING_TRADE_HOURS = {'evening': TradingSession(time(19, 5), time(23, 50))}
+STOCKS_TRADE_HOURS = {'base': TradingSession(time(10, 00), time(18, 39, 59))}
+FUTURES_TRADE_HOURS = {
+    'before_clearing': TradingSession(time(9, 00), time(14, 00)),
+    'after_clearing': TradingSession(time(14, 5), time(18, 45)),
 }
+TIME_OFFSET = timedelta(seconds=15)
+MOSCOW_ZONE = zoneinfo.ZoneInfo('Europe/Moscow')
 
 
 class TradingTime:
     def __init__(self, asset: Asset) -> None:
         self._asset = asset
         if asset.asset_type == 'F':
-            self._trading_hours = dict(FUTURES_TRADE_HOURS)
+            self._trading_sessions = dict(FUTURES_TRADE_HOURS)
         else:
-            self._trading_hours = dict(STOCKS_TRADE_HOURS)
-        if asset.morning_trading:
-            self._trading_hours.update(MORNING_TRADE_HOURS)
+            self._trading_sessions = dict(STOCKS_TRADE_HOURS)
+        if asset.morning_trading and asset.asset_type != 'F':
+            self._trading_sessions.update(MORNING_TRADE_HOURS)
         if asset.evening_trading:
-            self._trading_hours.update(EVENING_TRADE_HOURS)
+            self._trading_sessions.update(EVENING_TRADE_HOURS)
+        self._update_trading_schedule_with_time_offset()
+        self._sorted_sess_cache = None
 
     @property
     def is_trading_now(self) -> bool:
-        if self._today_is_a_trading_day:
-            for open_time, close_time in self._trading_hours.values():
-                if open_time <= self._current_time.time() <= close_time:
-                    return True
+        if not self._today_is_a_trading_day:
+            return False
+        for open_time, close_time in self._trading_sessions.values():
+            if open_time <= self._current_datetime.time() <= close_time:
+                return True
         return False
 
     @property
-    def _current_time(self) -> datetime:
-        return datetime.now(ZONE)
+    def _current_datetime(self) -> datetime:
+        return datetime.now(MOSCOW_ZONE)
 
     @property
     def _today_is_a_trading_day(self) -> bool:
-        return TRADE_DAYS[0] <= self._current_time.weekday() <= TRADE_DAYS[1]
+        return (
+            TRADE_DAYS[0] <= self._current_datetime.weekday() <= TRADE_DAYS[1]
+        )
+
+    @property
+    def seconds_till_trading_starts(self) -> int:
+        if self.is_trading_now:
+            return 0
+        if self._will_trade_today:
+            return self._seconds_until_next_session_today
+        return self._seconds_count_till_trading_after_session_close
+
+    @property
+    def _seconds_until_next_session_today(self) -> int:
+        return self._seconds_till_nearest_session(
+            self._current_datetime.time()
+        )
+
+    @property
+    def _seconds_count_till_trading_after_session_close(self) -> int:
+        midnights = self._midnights_count_till_trading_starts
+        secs_midn_to_session = self._seconds_till_nearest_session(time(0, 0))
+        if midnights > 0:
+            return (
+                self._seconds_till_midnight
+                + 24 * 60 * 60 * (midnights - 1)
+                + secs_midn_to_session
+            )
+        return self._seconds_till_midnight + secs_midn_to_session
+
+    @property
+    def _seconds_till_midnight(self) -> int:
+        return (
+            self._time_difference_in_seconds(
+                time(23, 59, 59), self._current_datetime.time()
+            )
+            + 1
+        )
+
+    def _seconds_till_nearest_session(self, tyme: time) -> int:
+        for session in self.sorted_sessions:
+            if session.open > tyme:
+                return self._time_difference_in_seconds(session.open, tyme)
+        return 0
+
+    def _time_difference_in_seconds(self, t1: time, t2: time):
+        delta = datetime.combine(
+            self._current_datetime.date(), t1
+        ) - datetime.combine(self._current_datetime.date(), t2)
+        return delta.seconds
+
+    @property
+    def sorted_sessions(self) -> list:
+        if not self._sorted_sess_cache:
+            self._sorted_sess_cache = sorted(self._trading_sessions.values())
+        return self._sorted_sess_cache
+
+    @property
+    def _midnights_count_till_trading_starts(self) -> int:
+        if self._will_trade_today:
+            return 0
+        midnights = 1
+        while (
+            self._weekday_ahead_number(midnights) > TRADE_DAYS[1]
+            or self._weekday_ahead_number(midnights) < TRADE_DAYS[0]
+        ):
+            midnights += 1
+        return midnights
+
+    @property
+    def _is_later_then_last_session_close(self) -> bool:
+        return self._current_datetime.time() >= self.sorted_sessions[-1].close
+
+    @property
+    def _will_trade_today(self) -> bool:
+        return (
+            not self._is_later_then_last_session_close
+            and self._today_is_a_trading_day
+        )
+
+    def _weekday_ahead_number(self, days_amount: int) -> int:
+        return (days_amount + self._current_datetime.weekday()) % 7
+
+    def _update_trading_schedule_with_time_offset(self) -> None:
+        self._trading_sessions = {
+            title: TradingSession(
+                open=(
+                    datetime.combine(
+                        self._current_datetime.date(), period.open
+                    )
+                    + TIME_OFFSET
+                ).time(),
+                close=(
+                    datetime.combine(
+                        self._current_datetime.date(), period.close
+                    )
+                    - TIME_OFFSET
+                ).time(),
+            )
+            for title, period in self._trading_sessions.items()
+        }
--- tests/bot_tests/classes_tests/trading_time_test.py
+++ tests/bot_tests/classes_tests/trading_time_test.py
@@ -2,15 +2,16 @@ from datetime import datetime
 
 import pytest
 
-from bot.tools.trading_time import ZONE, TradingTime
+from bot.tools.trading_time import MOSCOW_ZONE, TIME_OFFSET, TradingTime
 from tests.bot_tests.classes_tests.factories import AssetFactory
 
-NOON_HOUR = (12, 0, 0)
-MIDNIGHT_HOUR = (0, 0, 0)
-MORNING_HOUR = (9, 0, 0)
-EVENING_HOUR = (21, 0, 0)
-
-EXTRA_HOURS = pytest.mark.parametrize('hour', ((MORNING_HOUR), (EVENING_HOUR)))
+NOON = (12, 0, 0)
+MIDNIGHT = (0, 0, 0)
+MORNING = (9, 5, 0)
+EVENING = (21, 0, 0)
+AFTER_CLOSE = (23, 55, 0)
+OFFSET = TIME_OFFSET.seconds
+EXTRA_HOURS = pytest.mark.parametrize('hour', ((MORNING), (EVENING)))
 FUTURES_CLEARING_TIMES = ((14, 1, 0), (18, 55, 0), (19, 4, 0))
 STOCKS_CLEARING_TIMES = ((18, 45, 0), (18, 55, 0), (19, 2, 0))
 
@@ -22,57 +23,221 @@ STOCK_PARAMS = dict(
 )
 
 
-def generic_trading_time_test(day_of_month, time, asset_params={}):
-    dt = datetime(2023, 3, day_of_month, *time, tzinfo=ZONE)
-    asset = AssetFactory.build(**asset_params)
-    pytest.MonkeyPatch().setattr(TradingTime, '_current_time', dt)
-    return TradingTime(asset).is_trading_now
+class GenericTradingTimeTester:
+    def __init__(self, day, tyme, asset_params={}) -> None:
+        self._day = day
+        self._time = tyme
+        self._params = asset_params
+
+    @property
+    def create_tradingtime_object(self) -> TradingTime:
+        dt = datetime(2023, 3, self._day, *self._time, tzinfo=MOSCOW_ZONE)
+        asset = AssetFactory.build(**self._params)
+        pytest.MonkeyPatch().setattr(TradingTime, '_current_datetime', dt)
+        return TradingTime(asset)
+
+    @property
+    def is_trading_now(self) -> bool:
+        return self.create_tradingtime_object.is_trading_now
+
+    @property
+    def midnights_count_till_trading_starts(self) -> int:
+        return (
+            self.create_tradingtime_object._midnights_count_till_trading_starts
+        )
+
+    @property
+    def seconds_till_trading_starts(self) -> int:
+        return self.create_tradingtime_object.seconds_till_trading_starts
 
 
 @pytest.mark.parametrize('workday_of_month', ((1), (2), (3), (6), (7)))
 def test_noon_is_trading_time_on_workdays(workday_of_month):
-    assert generic_trading_time_test(workday_of_month, NOON_HOUR) is True
+    assert (
+        GenericTradingTimeTester(workday_of_month, NOON).is_trading_now is True
+    )
 
 
 @pytest.mark.parametrize('day_of_month', ((1), (2), (3), (4), (5), (6), (7)))
 def test_midnight_is_not_trading_time(day_of_month):
-    assert generic_trading_time_test(day_of_month, MIDNIGHT_HOUR) is False
+    assert (
+        GenericTradingTimeTester(day_of_month, MIDNIGHT).is_trading_now
+        is False
+    )
 
 
 @pytest.mark.parametrize('weekend_day_of_month', ((4), (5)))
 def test_weekends_are_not_trading(weekend_day_of_month):
-    assert generic_trading_time_test(weekend_day_of_month, NOON_HOUR) is False
+    assert (
+        GenericTradingTimeTester(weekend_day_of_month, NOON).is_trading_now
+        is False
+    )
 
 
 @pytest.mark.parametrize(
     ('hour', 'm_trading', 'e_trading', 'result'),
     (
-        (MORNING_HOUR, True, False, True),
-        (MORNING_HOUR, False, True, False),
-        (EVENING_HOUR, True, False, False),
-        (EVENING_HOUR, True, True, True),
+        (MORNING, True, False, True),
+        (MORNING, False, True, False),
+        (EVENING, True, False, False),
+        (EVENING, True, True, True),
     ),
 )
-def test_trading_time_on_morn_and_evn_sess(hour, m_trading, e_trading, result):
-    params = dict(morning_trading=m_trading, evening_trading=e_trading)
-    assert generic_trading_time_test(1, hour, params) == result
+def test_stock_trading_time_on_morn_and_evn(
+    hour, m_trading, e_trading, result
+):
+    params = dict(
+        morning_trading=m_trading, evening_trading=e_trading, asset_type='S'
+    )
+    assert GenericTradingTimeTester(1, hour, params).is_trading_now is result
 
 
 @EXTRA_HOURS
 def test_generic_stock_not_trading_extra_hours(hour):
-    assert generic_trading_time_test(1, hour, STOCK_PARAMS) is False
+    assert (
+        GenericTradingTimeTester(1, hour, STOCK_PARAMS).is_trading_now is False
+    )
 
 
 @EXTRA_HOURS
 def test_generic_future_trading_extra_hours(hour):
-    assert generic_trading_time_test(1, hour, FUTURE_PARAMS) is True
+    assert (
+        GenericTradingTimeTester(1, hour, FUTURE_PARAMS).is_trading_now is True
+    )
 
 
 @pytest.mark.parametrize('clearing_hour', FUTURES_CLEARING_TIMES)
 def test_future_not_trading_on_clearing(clearing_hour):
-    assert generic_trading_time_test(1, clearing_hour, FUTURE_PARAMS) is False
+    assert (
+        GenericTradingTimeTester(
+            1, clearing_hour, FUTURE_PARAMS
+        ).is_trading_now
+        is False
+    )
 
 
 @pytest.mark.parametrize('clearing_hour', STOCKS_CLEARING_TIMES)
 def test_stock_not_trading_on_clearing(clearing_hour):
-    assert generic_trading_time_test(1, clearing_hour, STOCK_PARAMS) is False
+    assert (
+        GenericTradingTimeTester(1, clearing_hour, STOCK_PARAMS).is_trading_now
+        is False
+    )
+
+
+def test_midnights_till_trading_starts_on_tradingtime():
+    assert (
+        GenericTradingTimeTester(
+            6, NOON, STOCK_PARAMS
+        ).midnights_count_till_trading_starts
+        == 0
+    )
+
+
+@pytest.mark.parametrize('time', (MIDNIGHT, MORNING, NOON, EVENING))
+def test_midnights_till_trading_starts_before_close(time):
+    assert (
+        GenericTradingTimeTester(
+            6, time, FUTURE_PARAMS
+        ).midnights_count_till_trading_starts
+        == 0
+    )
+
+
+@pytest.mark.parametrize(
+    ('day', 'result'), ((1, 0), (2, 0), (3, 0), (4, 2), (5, 1), (6, 0), (7, 0))
+)
+def test_midnights_till_trading_starts_noon(day, result):
+    assert (
+        GenericTradingTimeTester(
+            day, NOON, FUTURE_PARAMS
+        ).midnights_count_till_trading_starts
+        == result
+    )
+
+
+@pytest.mark.parametrize(
+    ('day', 'result'), ((1, 1), (2, 1), (3, 3), (4, 2), (5, 1), (6, 1), (7, 1))
+)
+def test_midnights_till_trading_starts_after_close(day, result):
+    assert (
+        GenericTradingTimeTester(
+            day, AFTER_CLOSE, FUTURE_PARAMS
+        ).midnights_count_till_trading_starts
+        == result
+    )
+
+
+@pytest.mark.parametrize(
+    ('day', 'result'), ((1, 1), (2, 5), (3, 4), (4, 3), (5, 2), (6, 1), (7, 1))
+)
+def test_midnights_till_trading_with_other_tradedays(day, result, monkeypatch):
+    monkeypatch.setattr('bot.tools.trading_time.TRADE_DAYS', (1, 3))
+    assert (
+        GenericTradingTimeTester(
+            day, AFTER_CLOSE, FUTURE_PARAMS
+        ).midnights_count_till_trading_starts
+        == result
+    )
+
+
+def test_stocks_seconds_till_open_is_zero_on_trading_time():
+    assert (
+        GenericTradingTimeTester(
+            6, NOON, STOCK_PARAMS
+        ).seconds_till_trading_starts
+        == 0
+    )
+
+
+def test_future_seconds_till_trading_is_open_from_monday_midnight():
+    assert (
+        GenericTradingTimeTester(
+            6, MIDNIGHT, FUTURE_PARAMS
+        ).seconds_till_trading_starts
+        == 9 * 60 * 60 + OFFSET
+    )
+
+
+@pytest.mark.parametrize(
+    ('pause_time', 'asset_type', 'result'),
+    (
+        ((19, 0, 0), FUTURE_PARAMS, 5 * 60 + OFFSET),
+        ((9, 00), STOCK_PARAMS, 60 * 60 + OFFSET),
+        ((8, 00), FUTURE_PARAMS, 60 * 60 + OFFSET),
+        (MIDNIGHT, STOCK_PARAMS, 10 * 60 * 60 + OFFSET),
+        ((14, 3, 30), FUTURE_PARAMS, 90 + OFFSET),
+        ((14, 3, 30), STOCK_PARAMS, 0),
+        ((10, 00), STOCK_PARAMS, OFFSET),
+        ((10, 00), FUTURE_PARAMS, 0),
+    ),
+)
+def test_assets_seconds_till_trading_between_sessions(
+    pause_time, asset_type, result
+):
+    assert (
+        GenericTradingTimeTester(
+            6, pause_time, asset_type
+        ).seconds_till_trading_starts
+        == result
+    )
+
+
+@pytest.mark.parametrize(
+    ('day', 'tyme', 'result'),
+    (
+        (3, (23, 51), 2 * 24 * 60 * 60 + 9 * 60 + 10 * 60 * 60),
+        (4, (12, 00), 2 * 24 * 60 * 60 - 2 * 60 * 60),
+        (5, (15, 00), 19 * 60 * 60),
+        (6, (23, 50), 10 * 60 + 10 * 60 * 60),
+        (4, (0, 0), 2 * 24 * 60 * 60 + 10 * 60 * 60),
+        (5, (0, 0), 34 * 60 * 60),
+        (6, (0, 0), 10 * 60 * 60)
+    ),
+)
+def test_stock_seconds_before_open_through_weekends(day, tyme, result):
+    assert (
+        GenericTradingTimeTester(
+            day, tyme, STOCK_PARAMS
+        ).seconds_till_trading_starts
+        == result + OFFSET
+    )
